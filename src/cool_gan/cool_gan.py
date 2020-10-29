65edcd14bbc686bbd0facd0092b10fc38cb8b287
clbeggs <beggs240@gmail.com>
2020-10-29 16:13:36 -0600

    Created src/, with sample layout of project

---
+++ src/cool_gan/cool_gan.py
@@ -0,0 +1,20 @@
+import torch
+
+
+class ACoolGAN(torch.nn.Module):
+    """ 
+        Our Implementation of a GAN
+
+            Generator Architecture:
+
+            Discriminator Architecture:
+
+            Objective Function:
+    """
+
+    def __init__(self):
+        super(ACoolGAN, self).__init__()
+        raise NotImplementedError
+
+    def forward(self, x):
+        raise NotImplementedError
---
+++ src/simple_viz/simple_viz.py
@@ -0,0 +1,13 @@
+import torch
+from manimlib.imports import *
+
+
+
+"""
+Training visualization of simple GAN with manimlib
+"""
+
+
+
+
+
